52ca2578b6aeb2c4e6e26938001141e591b8e452
M4sterM4tt <WAIM1_16@uni.worc.ac.uk>
2018-02-06 13:36:01 +0000

    updateBall

--- www/js/index.js
+++ www/js/index.js
@@ -24,6 +24,9 @@ window.onload = function() {
 	
 	// Canvas and Graphics context
 	canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+	
 	body = canvas.getContext("2d");
 	time = 1;
 	
@@ -39,7 +42,10 @@ window.onload = function() {
 	
 	
 	body.beginPath();	
+	body.clearRect(0,0,canvas.width,canvas.height);
 	body.drawImage(Level[0],0,0,body.width,body.height);
+	
+	body.beginPath();
 	body.drawImage(playerImages[1],playerImagesX[1],playerImagesY[1],body.width/20,body.width/20);
 	
 	window.setInterval(render,time);
@@ -50,19 +56,24 @@ window.onload = function() {
 
 
 function render(event) {
-  playerImagesX[0] = event.beta;  // In degree in the range [-180,180]
-  playerImagesY[0] = event.gamma; // In degree in the range [-90,90]
+	playerImagesX[0] = event.beta;  // In degree in the range [-180,180]
+	playerImagesY[0] = event.gamma; // In degree in the range [-90,90]
 
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
+	// Because we don't want to have the device upside down
+	// We constrain the x value to the range [-90,90]
+	if (x >  90) { x =  90};
+	if (x < -90) { x = -90};
 
-  // To make computation easier we shift the range of x and y to [0,180]
-  x += 90;
-  y += 90;
-
-  body.drawImage(playerImages[1],playerImagesX[1],playerImagesY[1],body.width/20,body.width/20);
+	// To make computation easier we shift the range of x and y to [0,180]
+	x += 90;
+	y += 90;
+	
+	body.beginPath();
+	body.clearRect(0,0,canvas.width,canvas.height);
+	body.drawImage(Level[0],0,0,body.width,body.height);
+	
+	body.beginPath();
+	body.drawImage(playerImages[1],playerImagesX[1],playerImagesY[1],body.width/20,body.width/20);
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
