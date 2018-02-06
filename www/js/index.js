e68b7569bfddb19fcc2cbaa422949b44bb7dfb30
M4sterM4tt <WAIM1_16@uni.worc.ac.uk>
2018-02-06 14:01:28 +0000

    ballUpdate

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,12 @@
 
 // Step One
 // The Player Ball changes it's position because the player tilts the device. NEED TO REFRENCE THIS (https://developer.mozilla.org/en-US/docs/Web/API/Detecting_device_orientation)
+// NEED TO REFRENCE THIS https://mobiforge.com/design-development/html5-mobile-web-canvas
 
 
 // Canvas Variables 
 var canvas; 
 var body;
-var time;
 
 // Image Variables
 var Level; 
@@ -17,7 +17,8 @@ var playerImages;
 var playerImagesX;
 var playerImagesY;
 
-
+// Event Listeners
+window.addEventListener('deviceorientation', handleOrientation);
 
 
 window.onload = function() {
@@ -26,13 +27,11 @@ window.onload = function() {
 	canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-	
 	body = canvas.getContext("2d");
-	time = 1;
 	
 	
 	// Images and variables for Images
-	Level = [document.getElementById("levelBase"), document.getElementById("levelOne"), document.getElementById("levelTwo"), document.getElementById("levelThree"), document.getElementById("levelFour"), document.getElementById("levelFive")];
+	Level = document.getElementById("levelBase");
 	playerImages = [document.getElementById("playerGoal"), document.getElementById("playerBall")];
 	
 	
@@ -42,41 +41,35 @@ window.onload = function() {
 	
 	
 	body.beginPath();	
-	body.clearRect(0,0,canvas.width,canvas.height);
-	body.drawImage(Level[0],0,0,body.width,body.height);
+	body.drawImage(Level,0,0,canvas.width,canvas.height);
 	
 	body.beginPath();
-	body.drawImage(playerImages[1],playerImagesX[1],playerImagesY[1],body.width/20,body.width/20);
-	
-	window.setInterval(render,time);
-	render();	
+	body.drawImage(playerImages[1],playerImagesX[1],playerImagesY[1],body.width/20 + body.width/10,body.width/20 + body.width/10);	
 }
 
 
 
 
-function render(event) {
+function handleOrientation(event) {
 	playerImagesX[0] = event.beta;  // In degree in the range [-180,180]
 	playerImagesY[0] = event.gamma; // In degree in the range [-90,90]
 
 	// Because we don't want to have the device upside down
 	// We constrain the x value to the range [-90,90]
-	if (x >  90) { x =  90};
-	if (x < -90) { x = -90};
+	if (playerImagesX[0] >  90) {playerImagesX[0] =  90};
+	if (playerImagesX[0] < -90) {playerImagesX[0] = -90};
 
 	// To make computation easier we shift the range of x and y to [0,180]
-	x += 90;
-	y += 90;
+	playerImagesX[0] += 90;
+	playerImagesY[0] += 90;
 	
 	body.beginPath();
 	body.clearRect(0,0,canvas.width,canvas.height);
-	body.drawImage(Level[0],0,0,body.width,body.height);
+	body.drawImage(Level,0,0,body.width,body.height);
 	
 	body.beginPath();
 	body.drawImage(playerImages[1],playerImagesX[1],playerImagesY[1],body.width/20,body.width/20);
 }
-
-window.addEventListener('deviceorientation', handleOrientation);
 
 
 
